95ee7acdbe7484f9e7e36d6f1ca7eb1930d03ef3
Marllon Neves <marllonneves@fiotec.fiocruz.br>
2025-05-06 20:01:49 -0300

    Feat: Criando e estruturando a solução do projeto em arquitetura de camadas baseadas em DDD e criando as entidades do negócio.

---
+++ ProdutosApp.Domain/Entities/Categoria.cs
@@ -0,0 +1,18 @@
+﻿namespace ProdutosApp.Domain.Entities
+{
+    /// <summary>
+    /// Modelo de entidade de domínio para Categoria
+    /// </summary>
+    public class Categoria
+    {
+        #region Propriedades
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+        public bool? Ativo { get; set; }
+        #endregion
+
+        #region Relacionamento
+        public ICollection<Produto>? Produtos { get; set; }
+        #endregion
+    }
+}
---
+++ ProdutosApp.Domain/Entities/Produto.cs
@@ -0,0 +1,22 @@
+﻿namespace ProdutosApp.Domain.Entities
+{
+    /// <summary>
+    /// Modelo de entidade de domínio para Produto
+    /// </summary>
+    public class Produto
+    {
+        #region Propriedades
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+        public decimal? Preco { get; set; }
+        public int? Quantidade { get; set; }
+        public DateTime? DataHoraCriacao { get; set; }
+        public bool? Ativo { get; set; }
+        public Guid? CategoriaId { get; set; }
+        #endregion
+
+        #region Relacionamentos
+        public Categoria? Categoria { get; set; }
+        #endregion
+    }
+}
