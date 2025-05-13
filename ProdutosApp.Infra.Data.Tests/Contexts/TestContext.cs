923fc377fb0be0283a995520f612bc28f652a1c7
Marllon Neves <marllonneves@fiotec.fiocruz.br>
2025-05-13 20:23:02 -0300

    Feat: Desenvolvimento do teste unitários da camada de repositório.

---
+++ ProdutosApp.Infra.Data.Tests/Contexts/TestContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutosApp.Infra.Data.Contexts;
+
+namespace ProdutosApp.Infra.Data.Tests.Contexts
+{
+    /// <summary>
+    /// Classe para contexto e preparação dos teste
+    /// </summary>
+    public class TestContext
+    {
+        /// <summary>
+        /// Método para configurar e retornar uma instância da classe DataContext
+        /// </summary>
+        public static DataContext CreateDataContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                    .UseInMemoryDatabase(databaseName: "ProdutosAppTest")
+                    .Options;
+
+            return new DataContext(options);
+        }
+    }
+}
