68031a57818d55f3b3b0910fb6915a7743031724
Marllon Neves <marllonneves@fiotec.fiocruz.br>
2025-05-13 22:07:37 -0300

    Feat: Implementando controllers e interface de serviço da aplicação para produto

---
+++ ProdutosApp.Application/Dtos/Requests/CategoriaRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ProdutosApp.Application.Dtos.Requests
+{
+    /// <summary>
+    /// Modelo de dados da resposta da aplicação para uma operação de categoria
+    /// </summary>
+    public class CategoriaRequest
+    {
+    }
+}
--- ProdutosApp.Application/Dtos/Requests/ProdutoRequest.cs
+++ ProdutosApp.Application/Dtos/Requests/ProdutoRequest.cs
@@ -1,15 +1,13 @@
-﻿namespace ProdutosApp.Application.Dtos.Responses
+﻿namespace ProdutosApp.Application.Dtos.Requests
 {
     /// <summary>
     /// Modelo de dados da resposta da aplicação para uma operação de produto
     /// </summary>
-    public class ProdutoResponse
+    public class ProdutoRequest
     {
-        public Guid? Id { get; set; }
         public string? Nome { get; set; }
         public decimal? Preco { get; set; }
         public int? Quantidade { get; set; }
-        public DateTime? DataHoraCriacao { get; set; }
         public Guid? CategoriaId { get; set; }
 
     }
