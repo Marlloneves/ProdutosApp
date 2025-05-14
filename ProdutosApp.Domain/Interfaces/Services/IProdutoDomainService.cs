1816fc11d6bd473092a48227f233692efa898a71
Marllon Neves <marllonneves@fiotec.fiocruz.br>
2025-05-13 21:22:59 -0300

    Feat: Implementando regra de negócio na ProdutoDomainService e extensions globais.

---
+++ ProdutosApp.Domain/Interfaces/Services/IProdutoDomainService.cs
@@ -0,0 +1,16 @@
+﻿using ProdutosApp.Domain.Entities;
+
+namespace ProdutosApp.Domain.Interfaces.Services
+{
+    /// <summary>
+    /// Interface para operações de serviço de domínio de produto
+    /// </summary>
+    public interface IProdutoDomainService
+    {
+        Task Adicionar(Produto produto);
+        Task Atualizar(Produto produto);
+        Task<Produto> Inativar(Guid? id);
+        Task<List<Produto>> ObterTodos();
+        Task<Produto?> ObterPorId(Guid? id);
+    }
+}
